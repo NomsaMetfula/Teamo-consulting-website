1226c280c78318dc6b4cf087dbdca2ff44e08a2b
Nomsa Metfula <Nomsatfula@gmail.com>
2026-01-07 09:11:56 +0200

    initial commit

---
+++ src/main/java/com/mycompany/teamoconsultingwebsite/TeamoConsultingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.teamoconsultingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamoConsultingWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamoConsultingWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/teamoconsultingwebsite/TeamoConsultingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.teamoconsultingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamoConsultingWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
